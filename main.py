1d7fb4738116c97b4410ed6627ac52d714730ae6
ggtimurr <timur23@kstu.kg>
2025-09-23 19:52:53 +0600

    check

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ def main(page: ft.Page):
     month_tab_content = build_month_tab(page)
 
     # Добавляем на страницу
-    # page.add(week_tab_content)
-    page.add(month_tab_content)
+    page.add(week_tab_content)
+    # page.add(month_tab_content)
 
 if __name__ == "__main__":
     ft.app(target=main)
